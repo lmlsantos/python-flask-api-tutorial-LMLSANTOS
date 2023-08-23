e005d6a2944f8c565b3fa03ae64eea958be3c6c3
lmlsantos <126293567+lmlsantos@users.noreply.github.com>
2023-08-23 11:25:38 +0000

    completed

--- src/app.py
+++ src/app.py
@@ -3,28 +3,41 @@ from flask import request
 
 app = Flask(__name__)
 
-# data to be stored (todo list)
+# Data to be JSON serialize
 todos = [
-    {"label": "My first task", "done": False},
-    {"label":"My second task", "done": False},
-    {"label":"My third task", "done": False}
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
 ]
 
-#creating the first endpoint"
+# The main page
+@app.route('/')
+def type():
+    return 'Type in /todos to get transfered to the url'
+
+
+#  The first endpoint GET
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    #converting the variable todos into a json string, and return it to the frontend 
+def set_todos():
+    # Converting the above variable into a json string
     json_text = jsonify(todos)
     return json_text
 
+
+# The second endpoint POST
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-   request_body = request.data
-   print("Incoming request with the following body", request_body)
-   return 'Response for the POST todo'
-
-
-
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos), 200
+  
+   
+# The third endpoint DELETE
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    
+    del todos[position]
+    return jsonify(todos), 200
 
 
 
